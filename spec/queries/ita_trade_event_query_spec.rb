494ce8c626014f793ed08475c1d301925bccabb5
Calum Halcrow <calumhalcrow@gmail.com>
2014-09-03 19:51:01 +0800

    Move TradeEvent to ItaTradeEvent.
    [#77714604]

--- spec/queries/ita_trade_event_query_spec.rb
+++ spec/queries/ita_trade_event_query_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe TradeEventQuery do
-  let(:fixtures_dir) { "#{Rails.root}/spec/fixtures/trade_events" }
+describe ItaTradeEventQuery do
+  let(:fixtures_dir) { "#{Rails.root}/spec/fixtures/ita_trade_events" }
 
   describe '#new' do
     it_behaves_like 'a paginated query'
 
     context 'when options include countries' do
-      subject { TradeEventQuery.new(countries: 'us,ca') }
+      subject { described_class.new(countries: 'us,ca') }
 
       describe '#countries' do
         subject { super().countries }
@@ -22,7 +22,7 @@ describe TradeEventQuery do
     end
 
     context 'when options is an empty hash' do
-      let(:query) { TradeEventQuery.new({}) }
+      let(:query) { described_class.new({}) }
       let(:search_body) { JSON.parse open("#{fixtures_dir}/search_body_with_default_options.json").read }
 
       it 'generates search body with default options' do
@@ -31,7 +31,7 @@ describe TradeEventQuery do
     end
 
     context 'when options include industry' do
-      let(:query) { TradeEventQuery.new(industry: 'fishing') }
+      let(:query) { described_class.new(industry: 'fishing') }
       let(:search_body) { JSON.parse open("#{fixtures_dir}/search_body_with_match_industries.json").read }
 
       it 'generates search body with queries' do
@@ -40,7 +40,7 @@ describe TradeEventQuery do
     end
 
     context 'when options include q' do
-      let(:query) { TradeEventQuery.new(q: 'workboat') }
+      let(:query) { described_class.new(q: 'workboat') }
       let(:search_body) { JSON.parse open("#{fixtures_dir}/search_body_with_multi_match.json").read }
 
       it 'generates search body with queries' do
@@ -49,7 +49,7 @@ describe TradeEventQuery do
     end
 
     context 'when options include countries' do
-      let(:query) { TradeEventQuery.new(countries: 'IL, US') }
+      let(:query) { described_class.new(countries: 'IL, US') }
       let(:search_body) { JSON.parse open("#{fixtures_dir}/search_body_with_filters.json").read }
 
       it 'generates search body with filters' do
