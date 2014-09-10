cfce7fb0e849c7340725432c04f0033c1f9b65df
Calum Halcrow <calumhalcrow@gmail.com>
2014-09-10 17:45:16 +0800

    Reorganize TradeEvents::Ita specs.

--- spec/controllers/api/v1/ita_trade_events_controller_spec.rb
+++ spec/controllers/api/v1/ita_trade_events_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Api::V1::ItaTradeEventsController, type: :controller do
+describe Api::V1::TradeEvents::ItaController, type: :controller do
   describe '#search' do
     let(:search_params) do
       { 'countries' => 'MX',
@@ -12,7 +12,7 @@ describe Api::V1::ItaTradeEventsController, type: :controller do
     let(:search) { double('search') }
 
     before do
-      expect(ItaTradeEvent).to receive(:search_for).with(search_params).and_return(search)
+      expect(TradeEvent::Ita).to receive(:search_for).with(search_params).and_return(search)
       get :search,
           bogus_param: 'bogus value',
           countries:   'MX',
