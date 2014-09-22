76484b82e488e6e6bec52b93bb5f439b0276fe6a
Carlos Lima <carlos@cpan.org>
2014-09-22 18:51:48 +0000

    italocations uses new setup query method

--- app/queries/ita_office_location_query.rb
+++ app/queries/ita_office_location_query.rb
@@ -1,45 +1,8 @@
 class ItaOfficeLocationQuery < Query
-  def initialize(options)
-    super(options)
-    @country = options[:country].downcase if options[:country].present?
-    @city = options[:city].downcase if options[:city].present?
-    if options[:state].present?
-      @state = options[:state].downcase
-      @country ||= 'us'
-    end
-    @q = options[:q] if options[:q].present?
-    @sort = 'post.sort'
-  end
-
-  private
-
-  def generate_query(json)
-    json.query do
-      generate_multi_match(json, [:post, :office_name], @q) if @q
-      generate_match(json, :city, @city) if @city
-    end if @q || @city
-  end
-
-  def generate_filter(json)
-    json.filter do
-      json.bool do
-        json.must do
-          json.child! { json.term { json.country @country } } if country_search?
-          json.child! { json.term { json.state @state } } if state_search?
-        end
-      end
-    end if has_filter_options?
-  end
-
-  def has_filter_options?
-    country_search? || state_search?
-  end
-
-  def country_search?
-    @country
-  end
-
-  def state_search?
-    @state && @country && @country == 'us'
-  end
+  setup_query(
+    q:      %i(post office_name),
+    query:  %i(city),
+    filter: %i(country state),
+    sort:   %i(post.sort),
+  )
 end
