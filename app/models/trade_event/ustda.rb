7ec65772382e42060f067bf9c133a4085d5f9470
Carlos Lima <carlos@cpan.org>
2014-10-13 18:05:19 +0000

    ustda: mapping adjustment

--- app/models/trade_event/ustda.rb
+++ app/models/trade_event/ustda.rb
@@ -1,33 +1,13 @@
 module TradeEvent
   class Ustda
     extend ::Indexable
-    include Mappable
+    include TradeEvent::Mappable
 
-    self.settings = {
-      index: {
-        analysis: {
-          analyzer: { custom_analyzer: { type:      'custom',
-                                         tokenizer: 'standard',
-                                         filter:    %w(standard asciifolding lowercase snowball) },
-          },
-        },
-      },
-    }.freeze
-
-    self.mappings = {
-      ustda_event: {
-        _ttl:       { enabled: true },
-        properties: {
-          :'venues.country' => { type: 'string', analyzer: 'keyword' },
-          description: { type: 'string', analyzer: 'custom_analyzer' },
-          end_date:    { type: 'date', format: 'YYYY-MM-dd' },
-          event_name:  { type: 'string', analyzer: 'custom_analyzer' },
-          industries:  { type: 'string', analyzer: 'custom_analyzer' },
-          start_date:  { type: 'date', format: 'YYYY-MM-dd' },
-          id:          { type: 'string', index: :not_analyzed, include_in_all: false },
-        },
-      },
-    }.freeze
+    self.mappings = self.mappings.dup
+    self.mappings[to_s.typeize][:properties].merge!({
+      :'venues.country' => { type: 'string', analyzer: 'keyword' },
+      :'venues.city' => { type: 'string', analyzer: 'custom_analyzer' },
+    }).freeze
 
     self.source = 'USTDA'
   end
