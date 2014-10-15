c892e7b852ab976c0b46a1e2cab0d3ea6d40caa9
Carlos Lima <carlos@cpan.org>
2014-10-15 18:09:35 +0000

    ustda: make results appear on consolidated interface

--- app/models/trade_event/ustda.rb
+++ app/models/trade_event/ustda.rb
@@ -2,13 +2,12 @@ module TradeEvent
   class Ustda
     extend ::Indexable
     include TradeEvent::Mappable
+    self.source = 'USTDA'
 
     self.mappings = self.mappings.dup
     self.mappings[to_s.typeize][:properties].merge!({
       :'venues.country' => { type: 'string', analyzer: 'keyword' },
       :'venues.city' => { type: 'string', analyzer: 'custom_analyzer' },
     }).freeze
-
-    self.source = 'USTDA'
   end
 end
