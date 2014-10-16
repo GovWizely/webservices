6972a69104514494d95be71eddbc03e7e189ba22
Tim Hammer <timh@govwizely.com>
2014-10-16 14:31:39 -0400

    Added specs.

---
+++ spec/api/v1/sharepoint_trade_articles_spec.rb
@@ -0,0 +1,309 @@
+require 'spec_helper'
+
+describe 'Sharepoint Trade Article API V1' do
+  before(:all) do
+    SharepointTradeArticle.recreate_index
+    SharepointTradeArticleData.new("#{Rails.root}/spec/fixtures/sharepoint_trade_articles/articles/%d.xml").import
+  end
+
+  let(:search_path) { '/sharepoint_trade_articles/search' }
+  let(:v1_headers) { { 'Accept' => 'application/vnd.tradegov.webservices.v1' } }
+  let(:expected_results) { YAML.load_file("#{Rails.root}/spec/fixtures/sharepoint_trade_articles/results.yaml") }
+
+  describe 'GET /sharepoint_trade_articles/search.json' do
+
+    context 'when search parameters are empty' do
+      before { get search_path, { size: 50 }, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 4
+
+        results = json_response[:results]
+        results.should match_array expected_results
+
+      end
+    end
+
+    #context 'when q is specified' do
+    #  before { get search_path, { q: 'tpcc' }, v1_headers }
+    #  subject { response }
+
+    #  it_behaves_like 'a successful search request'
+
+    #  it 'returns parature faqs' do
+    #    json_response = JSON.parse(response.body, symbolize_names: true)
+    #    json_response[:total].should == 1
+
+    #    results = json_response[:results]
+    #    results[0].should == expected_results[1]
+
+    #  end
+    #end
+
+    context 'when title is specified' do
+      before { get search_path, { title: 'ata' }, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 1
+
+        results = json_response[:results]
+        results.should include expected_results[2]
+      end
+    end
+
+    context 'when short_title is specified' do
+      before { get search_path, { short_title: 'ata' }, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 1
+
+        results = json_response[:results]
+        results.should include expected_results[2]
+      end
+    end
+
+    context 'when summary is specified' do
+      before { get search_path, { summary: 'advocacy' }, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 1
+
+        results = json_response[:results]
+        results.should include expected_results[1]
+      end
+    end
+
+    context 'when creation_date_start or creation_date_end is specified' do
+      before { get search_path, { creation_date_start: '2014-08-27', creation_date_end: '2014-08-28' }, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 3
+
+        results = json_response[:results]
+        results.should include expected_results[0]
+        results.should include expected_results[2]
+        results.should include expected_results[3]
+      end
+    end
+
+    context 'when release_date_start or release_date_end is specified' do
+      before { get search_path, { release_date_start: '2014-08-27', release_date_end: '2014-08-28' }, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 0
+
+      end
+    end
+
+    context 'when expiration_date_start or expiration_date_end is specified' do
+      before { get search_path, { expiration_date_start: '2014-08-27', expiration_date_end: '2014-08-28' }, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 0
+
+      end
+    end
+
+    context 'when source_agency or source_business_units or source_offices is specified' do
+      before { get search_path, { source_agency: 'trade', source_business_units: 'markets', source_offices: 'director general' }, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 1
+
+        results = json_response[:results]
+        results.should include expected_results[0]
+      end
+    end
+
+    context 'when content is specified' do
+      before { get search_path, { content: 'foreign payment disputes' }, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 2
+
+        results = json_response[:results]
+        results[0].should == expected_results[1]
+        results[1].should == expected_results[2]
+      end
+    end
+
+    context 'when keyword is specified' do
+      before { get search_path, { keyword: 'dispute' }, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 1
+
+        results = json_response[:results]
+        results[0].should == expected_results[1]
+      end
+    end
+
+    context 'when export_phases is specified' do
+      before { get search_path, { export_phases: 'expand' }, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 1
+
+        results = json_response[:results]
+        results[0].should == expected_results[0]
+      end
+    end
+
+    context 'when industries is specified' do
+      before { get search_path, { industries: 'defense' }, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 1
+
+        results = json_response[:results]
+        results[0].should == expected_results[0]
+      end
+    end
+
+    context 'when countries is specified' do
+      before { get search_path, { countries: 'af,ao' }, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 1
+
+        results = json_response[:results]
+        results[0].should == expected_results[0]
+      end
+    end
+
+    context 'when topic or sub_topics is specified' do
+      before { get search_path, { topic: 'free trade', sub_topics: 'nafta' }, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 1
+
+        results = json_response[:results]
+        results[0].should == expected_results[0]
+      end
+    end
+
+    context 'when geo_region or geo_subregion is specified' do
+      before { get search_path, { geo_region: 'asia', geo_subregion: 'east' }, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 1
+
+        results = json_response[:results]
+        results[0].should == expected_results[0]
+      end
+    end
+
+    context 'when trade_regions is specified' do
+      before { get search_path, { trade_regions: 'asia'}, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 1
+
+        results = json_response[:results]
+        results[0].should == expected_results[0]
+      end
+    end
+
+    context 'when trade_programs is specified' do
+      before { get search_path, { trade_programs: 'advisory'}, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 1
+
+        results = json_response[:results]
+        results[0].should == expected_results[0]
+      end
+    end
+
+    context 'when trade_initiatives is specified' do
+      before { get search_path, { trade_initiatives: 'discover'}, v1_headers }
+      subject { response }
+
+      it_behaves_like 'a successful search request'
+
+      it 'returns sharepoint trade articles' do
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response[:total].should == 1
+
+        results = json_response[:results]
+        results[0].should == expected_results[0]
+      end
+    end
+
+
+
+  end
+
+end
---
+++ spec/queries/sharepoint_trade_article_query_spec.rb
@@ -0,0 +1,171 @@
+require 'spec_helper'
+
+describe SharepointTradeArticleQuery do
+  let(:fixtures_dir) { "#{Rails.root}/spec/fixtures/sharepoint_trade_articles" }
+
+  describe '#generate_search_body' do
+
+    context 'when options is an empty hash' do
+      let(:query) { SharepointTradeArticleQuery.new({}) }
+
+      it 'generates search body with default options' do
+        JSON.parse(query.generate_search_body).should == {}
+      end
+    end
+
+    context 'when options include title' do
+      let(:query) { SharepointTradeArticleQuery.new(title: 'ata') }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/match_title.json").read }
+
+      it 'generates search body with queries' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+    context 'when options include short_title' do
+      let(:query) { SharepointTradeArticleQuery.new(short_title: 'ata') }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/match_short_title.json").read }
+
+      it 'generates search body with queries' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+ 
+    context 'when options include summary' do
+      let(:query) { SharepointTradeArticleQuery.new(summary: 'advocacy') }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/match_summary.json").read }
+
+      it 'generates search body with queries' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+    context 'when options include creation_date filter' do
+      let(:query) { SharepointTradeArticleQuery.new(creation_date_start: '2014-08-27', creation_date_end: '2014-08-28') }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/creation_date_filter.json").read }
+
+      it 'generates search body with filters' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+    context 'when options include release_date filter' do
+      let(:query) { SharepointTradeArticleQuery.new(release_date_start: '2014-08-27', release_date_end: '2014-08-28') }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/release_date_filter.json").read }
+
+      it 'generates search body with filters' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+    context 'when options include expiration_date filter' do
+      let(:query) { SharepointTradeArticleQuery.new(expiration_date_start: '2014-08-27', expiration_date_end: '2014-08-28') }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/expiration_date_filter.json").read }
+
+      it 'generates search body with filters' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+    context 'when options include source_agency or source_business_units or source_offices' do
+      let(:query) { SharepointTradeArticleQuery.new(source_agency: 'trade', source_business_units: 'markets', source_offices: 'director general') }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/match_source_agencies_fields.json").read }
+
+      it 'generates search body with queries' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+    context 'when options include content' do
+      let(:query) { SharepointTradeArticleQuery.new(content: 'foreign payment disputes') }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/match_content.json").read }
+
+      it 'generates search body with queries' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+    context 'when options include keyword' do
+      let(:query) { SharepointTradeArticleQuery.new(keyword: 'dispute') }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/match_keyword.json").read }
+
+      it 'generates search body with queries' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+    context 'when options include export_phases' do
+      let(:query) { SharepointTradeArticleQuery.new(export_phases: 'expand') }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/match_export_phases.json").read }
+
+      it 'generates search body with queries' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+    context 'when options include industries' do
+      let(:query) { SharepointTradeArticleQuery.new(industries: 'defense') }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/match_industries.json").read }
+
+      it 'generates search body with queries' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+    context 'when options include countries' do
+      let(:query) { SharepointTradeArticleQuery.new(countries: 'af,ao') }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/filter_countries.json").read }
+
+      it 'generates search body with filters' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+    context 'when options include topic or subtopics' do
+      let(:query) { SharepointTradeArticleQuery.new(topic: 'free trade', sub_topics: 'nafta' ) }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/match_topics_fields.json").read }
+
+      it 'generates search body with queries' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+    context 'when options include geo_region or geo_subregion' do
+      let(:query) { SharepointTradeArticleQuery.new(geo_region: 'asia', geo_subregions: 'east'  ) }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/match_geo_regions_fields.json").read }
+
+      it 'generates search body with queries' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+    context 'when options include trade_regions' do
+      let(:query) { SharepointTradeArticleQuery.new(trade_regions: 'asia') }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/match_trade_regions.json").read }
+
+      it 'generates search body with queries' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+    context 'when options include trade_programs' do
+      let(:query) { SharepointTradeArticleQuery.new(trade_programs: 'advisory') }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/match_trade_programs.json").read }
+
+      it 'generates search body with queries' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+        context 'when options include trade_initiatives' do
+      let(:query) { SharepointTradeArticleQuery.new(trade_initiatives: 'discover') }
+      let(:search_body) { JSON.parse open("#{fixtures_dir}/queries/match_trade_initiatives.json").read }
+
+      it 'generates search body with queries' do
+        JSON.parse(query.generate_search_body).should == search_body
+      end
+    end
+
+
+  end
+end
