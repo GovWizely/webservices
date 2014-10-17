7713092c4e0f77f7b8e7d62100fad5a2a06a8adc
James Isaacs <jamesiisaacs@gmail.com>
2014-10-17 00:16:17 -0700

    Expanded specs.

--- spec/api/v1/tariff_rates/consolidated_spec.rb
+++ spec/api/v1/tariff_rates/consolidated_spec.rb
@@ -13,6 +13,9 @@ describe 'Consolidated Tariff Rates API V1' do
       it_behaves_like 'a successful search request'
 
       it_behaves_like 'it contains all TariffRate::Australia results'
+      it_behaves_like 'it contains all TariffRate::CostaRica results'
+      it_behaves_like 'it contains all TariffRate::ElSalvador results'
+      it_behaves_like 'it contains all TariffRate::Korea results'
       it_behaves_like 'it contains only results with sources' do
         let(:sources) { %w(AUSTRALIA COSTA_RICA EL_SALVADOR KOREA) }
       end
@@ -25,7 +28,31 @@ describe 'Consolidated Tariff Rates API V1' do
       it_behaves_like 'a successful search request'
       it_behaves_like 'it contains all TariffRate::Australia results'
       it_behaves_like 'it contains only results with sources' do
-        let(:sources) { %w(AUSTRALIA COSTA_RICA EL_SALVADOR KOREA) }
+        let(:sources) { %w(AUSTRALIA) }
+      end
+    end
+
+    context 'and is set to "COSTA_RICA" source' do
+      let(:params) { {sources: 'COSTA_RICA'} }
+      it_behaves_like 'it contains all TariffRate::CostaRica results'
+      it_behaves_like 'it contains only results with sources' do
+        let(:sources) { %w(COSTA_RICA) }
+      end
+    end
+
+    context 'and is set to "EL_SALVADOR" source' do
+      let(:params) { {sources: 'EL_SALVADOR'} }
+      it_behaves_like 'it contains all TariffRate::ElSalvador results'
+      it_behaves_like 'it contains only results with sources' do
+        let(:sources) { %w(EL_SALVADOR) }
+      end
+    end
+
+    context 'and is set to "KOREA" source' do
+      let(:params) { {sources: 'KOREA'} }
+      it_behaves_like 'it contains all TariffRate::Korea results'
+      it_behaves_like 'it contains only results with sources' do
+        let(:sources) { %w(KOREA) }
       end
     end
   end
