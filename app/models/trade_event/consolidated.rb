c0358339b774b9b3682318b7e442edcc4a22f039
Calum Halcrow <calumhalcrow@gmail.com>
2014-10-22 12:14:36 +0800

    Use full query class names.

--- app/models/trade_event/consolidated.rb
+++ app/models/trade_event/consolidated.rb
@@ -1,7 +1,7 @@
 module TradeEvent
   class Consolidated
     def self.search_for(options)
-      query = Query.new(options)
+      query = TradeEvent::Query.new(options)
       hits = ES.client.search(
           index: index_names(query.sources),
           body:  query.generate_search_body,
