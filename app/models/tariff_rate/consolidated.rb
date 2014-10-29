21a5afc1fa5dfc018aeaa162035d86fd699237a7
James Isaacs <jamesiisaacs@gmail.com>
2014-10-28 23:56:26 -0700

    Added fully qualified class name.

--- app/models/tariff_rate/consolidated.rb
+++ app/models/tariff_rate/consolidated.rb
@@ -1,7 +1,7 @@
 module TariffRate
   class Consolidated
     def self.search_for(options)
-      query = Query.new(options)
+      query = TariffRate::Query.new(options)
       hits = ES.client.search(
           index:  index_names(query.sources),
           body:   query.generate_search_body,
