4620d7389921c3a50e4858a107775a6610c2f96a
Calum Halcrow <calumhalcrow@gmail.com>
2015-03-28 17:42:44 +0800

    Developers must enter min 8 characters 1 alpha to register
    [#91124244]

--- spec/support/users.rb
+++ spec/support/users.rb
@@ -1,7 +1,7 @@
 def build_user(args = {})
   defaults = { email:                 "rod#{Time.now.to_f + rand(1_000_000)}@example.com",
-               password:              'p',
-               password_confirmation: 'p',
+               password:              'p4ssword',
+               password_confirmation: 'p4ssword',
                full_name:             'Rod',
                company:               "Rod's Corp",
                confirmed_at:          Time.now.utc }
