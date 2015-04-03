c46c688dfefaa5ceeae5e753f01b35fc9e55e4b7
Calum Halcrow <calumhalcrow@gmail.com>
2015-04-03 19:21:48 +0800

    Shorter methods in V2::MarketResearchQuery.

--- app/queries/v2/market_research_query.rb
+++ app/queries/v2/market_research_query.rb
@@ -2,7 +2,9 @@ class V2::MarketResearchQuery < CountryIndustryQuery
   def initialize(options = {})
     super
     @sort = @q ? nil : 'title.keyword'
-    @industries = options[:industries].split(',').map(&:strip) rescue nil
+
+    @industries = options[:industries].split(',').map(&:strip) if options[:industries].present?
+
     @expiration_date = options[:expiration_date] if options[:expiration_date].present?
     # Just to be sure, at this point, that no
     # filtering/sorting/scoring is being done on @industry
@@ -29,21 +31,25 @@ class V2::MarketResearchQuery < CountryIndustryQuery
         json.must do
           json.child! { json.terms { json.countries @countries } } if @countries
           generate_date_range(json, 'expiration_date', @expiration_date) if @expiration_date
-          json.child! do
-            json.bool do
-              json.set! 'should' do
-                Array(@industries).each do |ind|
-                  json.child! do
-                    json.query do
-                      generate_multi_match(json, %w(industries.mapped.keyword), ind)
-                    end
-                  end
-                end
-              end
-            end
-          end if @industries
+          generate_industries_bool(json, @industries) if @industries
         end
       end
-    end if @countries || @industries || @expiration_date
+    end if [@countries, @industries, @expiration_date].any?
+  end
+
+  def generate_industries_bool(json, industries)
+    json.child! do
+      json.bool do
+        json.set! 'should' do
+          Array(industries).each do |industry|
+            json.child! do
+              json.query do
+                generate_multi_match(json, %w(industries.mapped.keyword), industry)
+              end
+            end
+          end
+        end
+      end
+    end
   end
 end
