1617c46eb556b883f246b7f439023529288b4a04
baraabourghli <baraa.bourghli@gmail.com>
2015-04-18 17:11:38 +0800

    Reorg of supporting data files for importers/trade_lead/uk specs

--- spec/importers/trade_lead/uk_data_spec.rb
+++ spec/importers/trade_lead/uk_data_spec.rb
@@ -4,7 +4,7 @@ describe TradeLead::UkData do
   let(:fixtures_dir) { "#{Rails.root}/spec/fixtures/trade_leads/uk" }
   let(:fixtures_file) { "#{fixtures_dir}/uk_trade_leads.csv" }
   let(:importer) { described_class.new(fixtures_file) }
-  let(:expected) { YAML.load_file("#{fixtures_dir}/results.yaml") }
+  let(:expected) { YAML.load_file("#{File.dirname(__FILE__)}/uk/results.yaml") }
 
   it_behaves_like 'an importer which can purge old documents'
 
