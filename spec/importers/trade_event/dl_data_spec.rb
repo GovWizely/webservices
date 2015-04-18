65e18296fc729fb85675173e452e8a404f03c3eb
baraabourghli <baraa.bourghli@gmail.com>
2015-04-18 18:00:58 +0800

    Reorg of supporting data files for importers/trade_event/dl specs

--- spec/importers/trade_event/dl_data_spec.rb
+++ spec/importers/trade_event/dl_data_spec.rb
@@ -1,15 +1,13 @@
 require 'spec_helper'
 
-# http://www.state.gov/rss/channels/dl.xml
 describe TradeEvent::DlData do
-  let(:fixtures_dir) { "#{Rails.root}/spec/fixtures/trade_events/dl/" }
-  let(:resource) { "#{fixtures_dir}/trade_events.xml" }
+  let(:resource) { "#{Rails.root}/spec/fixtures/trade_events/dl/trade_events.xml" }
   let(:importer) { described_class.new(resource) }
 
   it_behaves_like 'an importer which can purge old documents'
 
   describe '#import' do
-    let(:expected) { YAML.load_file("#{fixtures_dir}trade_events.yaml") }
+    let(:expected) { YAML.load_file("#{File.dirname(__FILE__)}/dl/results.yaml") }
 
     it 'loads Direct Line trade events from specified resource' do
       expect(TradeEvent::Dl).to receive(:index) do |dl_trade_events|
