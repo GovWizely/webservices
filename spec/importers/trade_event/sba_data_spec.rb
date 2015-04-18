51adb19a6fe502c4ca79212c3c29acdf82fca94f
baraabourghli <baraa.bourghli@gmail.com>
2015-04-18 17:51:56 +0800

    Reorg of supporting data files for importers/trade_event/sba specs

--- spec/importers/trade_event/sba_data_spec.rb
+++ spec/importers/trade_event/sba_data_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe TradeEvent::SbaData do
-  let(:fixtures_dir) { "#{Rails.root}/spec/fixtures/trade_events/sba" }
-  let(:resource) { "#{fixtures_dir}/new_events_listing.xml?offset=0" }
+  let(:resource) { "#{Rails.root}/spec/fixtures/trade_events/sba/new_events_listing.xml?offset=0" }
   let(:importer) do
     described_class.new(resource,
                         { reject_if_ends_before: Date.parse('2013-01-11') },
@@ -12,8 +11,9 @@ describe TradeEvent::SbaData do
   it_behaves_like 'an importer which can purge old documents'
 
   describe '#import' do
-    let(:expected_batch_1) { YAML.load_file("#{fixtures_dir}/imported_batch_1.yaml") }
-    let(:expected_batch_2) { YAML.load_file("#{fixtures_dir}/imported_batch_2.yaml") }
+    let(:results_dir) { "#{File.dirname(__FILE__)}/sba" }
+    let(:expected_batch_1) { YAML.load_file("#{results_dir}/results_batch_1.yaml") }
+    let(:expected_batch_2) { YAML.load_file("#{results_dir}/results_batch_2.yaml") }
 
     it 'loads SBA trade events from specified resource' do
       expect(TradeEvent::Sba).to receive(:index) do |sba_trade_events|
