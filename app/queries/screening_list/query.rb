73cf818fb7806c57637b69c1af148bc2dcc6f0fc
James Isaacs <james@govwizely.com>
2015-05-07 16:19:08 -0700

    DRYed up query.rb

--- app/queries/screening_list/query.rb
+++ app/queries/screening_list/query.rb
@@ -36,151 +36,67 @@ module ScreeningList
           end if @q
 
           if @name
-            generate_score_query(json, @score)
+            generate_score_query(json)
           end
 
           if @address
-            generate_fuzzy_queries(json, %w(addresses.address addresses.city addresses.state addresses.postal_code addresses.country), @address, operator = :and)
+            generate_fuzziness_queries(json, %w(addresses.address addresses.city addresses.state addresses.postal_code addresses.country), @address, operator = :and)
           end
         end
       end if [@q, @name, @distance, @address].any?
     end
 
-    def generate_score_query(json, score)
-      score ||= 0
+    def generate_score_query(json)
+      keyword_fields = [
+        'name_idx.keyword', 'name_nostop.keyword',
+        'alt_names_idx.keyword', 'alt_names_no_stops.keyword',
+        'rev_name.keyword', 'trim_name.keyword', 'trim_rev_name.keyword',
+        'rev_alt_names.keyword', 'trim_alt_names.keyword', 'trim_rev_alt_names.keyword']
+
+      non_keyword_fields = [
+        'name_idx', 'name_nostop',
+        'alt_names_idx', 'alt_names_no_stops',
+        'rev_name', 'trim_name', 'trim_rev_name',
+        'rev_alt_names', 'trim_alt_names', 'trim_rev_alt_names']
+
+      all_fields = keyword_fields + non_keyword_fields
+
+      h = {
+        score_100: { fields: keyword_fields, fuzziness: 0, weight: 5 },
+        score_95:  { fields: all_fields, fuzziness: 0, weight: 5 },
+        score_90:  { fields: keyword_fields, fuzziness: 1, weight: 5 },
+        score_85:  { fields: all_fields, fuzziness: 1, weight: 5 },
+        score_80:  { fields: keyword_fields, fuzziness: 2, weight: 5 },
+        score_75:  { fields: all_fields, fuzziness: 2, weight: 75 }
+      }
 
       json.disable_coord true
       json.set! 'should' do
-        # 100
-        json.explain
-        json.child! do
-          json.function_score do
-            json.boost_mode 'replace'
-            json.query do
-              json.multi_match do
-                json.query @name
-                json.fields ['name_idx.keyword', 'name_nostop.keyword', 'alt_names_idx.keyword', 'alt_names_no_stops.keyword',
-                             'rev_name.keyword', 'trim_name.keyword', 'trim_rev_name.keyword',
-                             'rev_alt_names.keyword', 'trim_alt_names.keyword', 'trim_rev_alt_names.keyword']
-              end
-            end
-            json.functions do
-              json.child! { json.weight 5 }
-            end
-          end
-        end
 
-        # 95
-        json.child! do
-          json.function_score do
-            json.boost_mode 'replace'
-            json.query do
-              json.multi_match do
-                json.query @name
-                json.fields ['name_idx', 'name_nostop', 'alt_names_idx', 'alt_names_no_stops', 'rev_name', 'trim_name', 'trim_rev_name',
-                             'rev_alt_names', 'trim_alt_names', 'trim_rev_alt_names',
-                             'name_idx.keyword', 'name_nostop.keyword', 'alt_names_idx.keyword', 'alt_names_no_stops.keyword', 'rev_name.keyword',
-                             'trim_name.keyword', 'trim_rev_name.keyword', 'rev_alt_names.keyword', 'trim_alt_names.keyword', 'trim_rev_alt_names.keyword']
-                json.prefix_length 1
-                json.operator :and
+        h.each { |key, value|
+          json.child! do
+            json.function_score do
+              json.boost_mode 'replace'
+              json.query do
+                json.multi_match do
+                  json.query @name
+                  json.prefix_length 1
+                  json.operator :and
+                  json.fields value[:fields]
+                  json.fuzziness value[:fuzziness]
+                end
+              end
+              json.functions do
+                json.child! { json.weight value[:weight] }
               end
             end
-            json.functions do
-              json.child! { json.weight 5 }
-            end
           end
-        end
-
-        # 90
-        json.child! do
-          json.function_score do
-            json.boost_mode 'replace'
-            json.query do
-              json.multi_match do
-                json.query @name
-                json.fields ['name_idx.keyword', 'name_nostop.keyword', 'alt_names_idx.keyword', 'alt_names_no_stops.keyword',
-                             'rev_name.keyword', 'trim_name.keyword', 'trim_rev_name.keyword',
-                             'rev_alt_names.keyword', 'trim_alt_names.keyword', 'trim_rev_alt_names.keyword']
-                json.prefix_length 1
-                json.fuzziness 1
-                json.operator :and
-              end
-            end
-            json.functions do
-              json.child! { json.weight 5 }
-            end
-          end
-        end
-
-        # 85
-        json.child! do
-          json.function_score do
-            json.boost_mode 'replace'
-            json.query do
-              json.multi_match do
-                json.query @name
-                json.fields ['name_idx', 'name_nostop', 'alt_names_idx', 'alt_names_no_stops', 'rev_name', 'trim_name', 'trim_rev_name',
-                             'rev_alt_names', 'trim_alt_names', 'trim_rev_alt_names',
-                             'name_idx.keyword', 'name_nostop.keyword', 'alt_names_idx.keyword', 'alt_names_no_stops.keyword', 'rev_name.keyword',
-                             'trim_name.keyword', 'trim_rev_name.keyword', 'rev_alt_names.keyword', 'trim_alt_names.keyword', 'trim_rev_alt_names.keyword']
-                json.prefix_length 1
-                json.fuzziness 1
-                json.operator :and
-              end
-            end
-            json.functions do
-              json.child! { json.weight 5 }
-            end
-          end
-        end
-
-        # 80
-        json.child! do
-          json.function_score do
-            json.boost_mode 'replace'
-            json.query do
-              json.multi_match do
-                json.query @name
-                json.fields ['name_idx.keyword', 'name_nostop.keyword', 'alt_names_idx.keyword', 'alt_names_no_stops.keyword',
-                             'rev_name.keyword', 'trim_name.keyword', 'trim_rev_name.keyword',
-                             'rev_alt_names.keyword', 'trim_alt_names.keyword', 'trim_rev_alt_names.keyword']
-                json.prefix_length 1
-                json.fuzziness 2
-                json.operator :and
-              end
-            end
-            json.functions do
-              json.child! { json.weight 5 }
-            end
-          end
-        end
-
-        # 75
-        json.child! do
-          json.function_score do
-            json.boost_mode 'replace'
-            json.query do
-              json.multi_match do
-                json.query @name
-                json.fields ['name_idx', 'name_nostop', 'alt_names_idx', 'alt_names_no_stops', 'rev_name', 'trim_name', 'trim_rev_name',
-                             'rev_alt_names', 'trim_alt_names', 'trim_rev_alt_names',
-                             'name_idx.keyword', 'name_nostop.keyword', 'alt_names_idx.keyword', 'alt_names_no_stops.keyword', 'rev_name.keyword',
-                             'trim_name.keyword', 'trim_rev_name.keyword', 'rev_alt_names.keyword', 'trim_alt_names.keyword', 'trim_rev_alt_names.keyword']
-                json.prefix_length 1
-                json.fuzziness 2
-                json.operator :and
-              end
-            end
-            json.functions do
-              json.child! { json.weight 75 }
-            end
-          end
-        end
+        }
 
       end
     end
 
-    def generate_fuzzy_queries(json, fields, value, operator)
+    def generate_fuzziness_queries(json, fields, value, operator)
       json.set! 'should' do
         json.child! { generate_multi_match(json, fields, value, operator) }
         json.child! do
