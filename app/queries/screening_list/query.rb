f27427212ca811694eec699aac8b77bd718d00ae
James Isaacs <james@govwizely.com>
2015-05-21 01:46:43 -0700

    moved trim names

--- app/queries/screening_list/query.rb
+++ app/queries/screening_list/query.rb
@@ -55,14 +55,14 @@ module ScreeningList
     def generate_fuzzy_name_query(json)
       keyword_fields = [
         'name_idx.keyword', 'name_no_common.keyword', 'alt_names_idx.keyword', 'alt_names_no_common.keyword',
-        'rev_name.keyword', 'rev_no_common.keyword', 'rev_alt_names.keyword', 'rev_alt_no_common.keyword'
+        'rev_name.keyword', 'rev_no_common.keyword', 'rev_alt_names.keyword', 'rev_alt_no_common.keyword',
+        'trim_name', 'trim_name_no_common', 'trim_alt_names', 'trim_alt_no_common',
+        'trim_rev_name', 'trim_rev_name_no_common', 'trim_rev_alt_names', 'trim_rev_alt_no_common'
       ]
 
       non_keyword_fields = [
         'name_idx', 'name_no_common', 'alt_names_idx ', 'alt_names_no_common ',
-        'rev_name', 'rev_no_common', 'rev_alt_names ', 'rev_alt_no_common',
-        'trim_name', 'trim_name_no_common', 'trim_alt_names', 'trim_alt_no_common',
-        'trim_rev_name', 'trim_rev_name_no_common', 'trim_rev_alt_names', 'trim_rev_alt_no_common'
+        'rev_name', 'rev_no_common', 'rev_alt_names ', 'rev_alt_no_common'
       ]
 
       all_fields = non_keyword_fields + keyword_fields
