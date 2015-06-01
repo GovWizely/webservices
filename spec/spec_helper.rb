588b74c7df5c99404d99a3af8ca7d89c5bd7ca49
Calum Halcrow <calum@rrsoft.co>
2015-06-01 14:35:25 +0800

    Put CodeClimate coverage statements first.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,8 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 
 require 'simplecov'
 SimpleCov.start 'rails'
-
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
