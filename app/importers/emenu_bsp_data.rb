4f135e29e70efa85ab64f6844bf02fdc44aad0b8
Joe Landers <joe@rrsoft.co>
2015-06-02 16:58:19 +0200

    Remove category restrictions from Emenu Bsp

--- app/importers/emenu_bsp_data.rb
+++ app/importers/emenu_bsp_data.rb
@@ -14,60 +14,6 @@ class EmenuBspData
     'email'       => :company_email,
   }.freeze
 
-  CATEGORIES = [
-    'Accounting, Auditing and Tax Services',
-    'Advertising',
-    'Architecture, Interior Design and Furniture',
-    'Banking and Financial Services',
-    'Building and Construction Services',
-    'Business Administration Services',
-    'Business Associations',
-    'Business Consulting',
-    'Business Development',
-    'Car Services and Rentals',
-    'Chambers of Commerce',
-    'Computer and Internet Services',
-    'Customs Brokerage',
-    'Debt Collection',
-    'Distributors, Sales Agents and Importers',
-    'Education and Training Services',
-    'Electronic Components and Supplies',
-    'Engineering Services',
-    'Entertainment Services',
-    'Environmental Services',
-    'Event Management, Conference Equipment and Facilitation',
-    'Export Management',
-    'Graphic Design',
-    'Hospitals, Clinics and Health Services',
-    'Hotels and Meeting Facilities',
-    'Human Resources',
-    'Insurance Services',
-    'Legal Services',
-    'Manufacturing and Industrial Production Services',
-    'Market Research',
-    'Marketing, Public Relations and Sales',
-    'Mining and Oil and Gas Services',
-    'Office Furniture',
-    'Office Rental',
-    'Other Business Services',
-    'Patent and Trademark Law Services',
-    'Photographic Services',
-    'Printing and Publishing Services',
-    'Product Standards, Testing, and Certification',
-    'Real Estate Services',
-    'Regional Economic Development',
-    'Relocation services',
-    'Restaurants and Catering',
-    'Retailers',
-    'Security and Personal Safety',
-    'Telecommunications',
-    'Trade Show and Exhibition Services',
-    'Translation and Interpretation',
-    'Transportation, Freight Forwarder and Storage Services',
-    'Travel Facilitation',
-    'Vetting/Due Diligence',
-  ].freeze
-
   def initialize(resource = ENDPOINT)
     @resource = resource
   end
@@ -85,8 +31,7 @@ class EmenuBspData
     article[:ita_office] = article_hash['site']
     article[:contact_title] = article_hash['company']['contact']['title']
     article[:contact_name] = article_hash['company']['contact']['name']
-    category = article_hash['company']['category']['title']
-    article[:category] = CATEGORIES.include?(category) ? category : nil
+    article[:category] =  article_hash['company']['category']['title']
     article[:id] = Utils.generate_id(article, %i(company_name company_description))
     sanitize_entry(article)
   end
