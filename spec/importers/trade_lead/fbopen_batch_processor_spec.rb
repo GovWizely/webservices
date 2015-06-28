5fb96f1119663bc9a7dc3f1c445a6b3f698395a5
Carlos Lima <carlos@cpan.org>
2015-06-28 07:03:04 +0000

    Fix batch processor not resetting internal list after processing

--- spec/importers/trade_lead/fbopen_batch_processor_spec.rb
+++ spec/importers/trade_lead/fbopen_batch_processor_spec.rb
@@ -3,18 +3,27 @@ require 'spec_helper'
 BatchProcessor = TradeLead::FbopenImporter::BatchProcessor
 describe 'BatchProcessor' do
   describe '#process!' do
-    it 'should execute when reaching batch size' do
-      pentries = []
-      b = BatchProcessor.new(2) { |x| pentries = x }
-      b.queued_process(1)
-      b.queued_process(1)
-      expect(pentries).to be_eql [1,1]
-    end
     it 'should not execute before reaching batch size' do
       pentries = []
       b = BatchProcessor.new(2) { |x| pentries = x }
       b.queued_process(1)
       expect(pentries).to be_empty
+    end
+    it 'should execute when reaching batch size' do
+      pentries = []
+      b = BatchProcessor.new(2) { |x| pentries = x }
+      b.queued_process(1)
+      b.queued_process(1)
+      expect(pentries).to eq([1, 1])
+    end
+    it 'should process each batch independently' do
+      pentries = []
+      b = BatchProcessor.new(2) { |x| pentries = x }
+      b.queued_process(1)
+      b.queued_process(2)
+      b.queued_process(3)
+      b.queued_process(4)
+      expect(pentries).to eq([3, 4])
     end
   end
 end
