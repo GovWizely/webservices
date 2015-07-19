abf84ed6996c82379dd4e2293a298319e0ad0998
baraabourghli <baraa.bourghli@gmail.com>
2015-07-19 11:11:15 +0800

    no need for v2 data to be imported seperatly.

--- spec/support/envirotech_search_behavior.rb
+++ spec/support/envirotech_search_behavior.rb
@@ -3,25 +3,10 @@ shared_context 'all Envirotech fixture data' do
 end
 
 shared_context 'all Envirotech v2 fixture data' do
-  include_context 'Envirotech::Solution data v2'
+  include_context 'Envirotech::Solution data'
 end
 
 shared_context 'Envirotech::Solution data' do
-  before do
-    Envirotech::Solution.recreate_index
-    fixtures_file =  "#{Rails.root}/spec/fixtures/envirotech/solution_articles/solution_articles.json"
-    allow_any_instance_of(Envirotech::SolutionData).to receive(:fetch_data).and_return(JSON.parse File.open(fixtures_file).read)
-    Envirotech::SolutionData.new(fixtures_file).import
-
-    @all_possible_full_results ||= {}
-    @all_possible_full_results[Envirotech::Solution] = JSON.parse(open(
-      "#{File.dirname(__FILE__)}/envirotech/solution/all_results.json").read)
-
-    allow(Date).to receive(:current).and_return(Date.parse('2013-01-11'))
-  end
-end
-
-shared_context 'Envirotech::Solution data v2' do
   before do
     Envirotech::Solution.recreate_index
     fixtures_file =  "#{Rails.root}/spec/fixtures/envirotech/solution_articles/solution_articles.json"
