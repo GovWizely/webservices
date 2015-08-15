1c0d6161fd913370ac75e353bffbceb6d8c09d00
baraabourghli <baraa.bourghli@gmail.com>
2015-08-15 18:20:00 +0800

    If toolkit is not reachable fall back to file in /data & notify Airbrake

--- app/importers/envirotech/envirotech_toolkit_data.rb
+++ app/importers/envirotech/envirotech_toolkit_data.rb
@@ -8,9 +8,15 @@ module Envirotech
       Envirotech::ProviderData.new.import
 
       scraper_data = Envirotech::ToolkitScraper.new.all_issue_info
+      if scraper_data.blank?
+        Airbrake.notify(EnvirotechToolkitNotFound.new)
+        Envirotech::RegulationData.new.import
+        Envirotech::SolutionData.new.import
+      else
+        Envirotech::RegulationData.new(scraper_data).import
+        Envirotech::SolutionData.new(scraper_data).import
+      end
 
-      Envirotech::RegulationData.new(scraper_data).import
-      Envirotech::SolutionData.new(scraper_data).import
       Envirotech::ProviderSolutionData.new.import
     end
   end
