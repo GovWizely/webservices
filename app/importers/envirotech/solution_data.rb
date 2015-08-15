17af5c5fbd471a60ecfd9328521cef454d2b78e3
baraabourghli <baraa.bourghli@gmail.com>
2015-08-15 17:52:58 +0800

    read from local file if no remote data provided

--- app/importers/envirotech/solution_data.rb
+++ app/importers/envirotech/solution_data.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 module Envirotech
   class SolutionData < Envirotech::BaseData
     include Importable
@@ -19,7 +21,11 @@ module Envirotech
     def initialize(relation_data = RELATION_DATA, resource = ENDPOINT)
       @resource = resource
       # get data from file if relation_data == RELATION_DATA
-      @relation_data = relation_data
+      if relation_data == RELATION_DATA
+        @relation_data = JSON.parse(open(relation_data).read)
+      else
+        @relation_data = relation_data
+      end
     end
 
     def import
@@ -40,8 +46,8 @@ module Envirotech
       article[:source] = model_class.source[:code]
 
       article[:id] = Utils.generate_id(article, %i(source_id source))
+      article[:issue_id] = @relation_data.select{ |_,v| v.with_indifferent_access[:solutions].include?(article[:name_english]) }.keys.map(&:to_i)
 
-      article[:issue_id] = @relation_data.select{ |_,v| v[:solutions].include?(article[:name_english]) }.keys
       sanitize_entry(article)
     end
   end
