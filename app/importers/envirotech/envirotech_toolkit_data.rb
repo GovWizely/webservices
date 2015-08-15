312ac6efeada4571bc706a98837ecf5f6b8fe9cb
baraabourghli <baraa.bourghli@gmail.com>
2015-08-15 16:52:04 +0800

    Adding issues IDs to regulation index

--- app/importers/envirotech/envirotech_toolkit_data.rb
+++ app/importers/envirotech/envirotech_toolkit_data.rb
@@ -6,8 +6,11 @@ module Envirotech
       Envirotech::BackgroundLinkData.new.import
       Envirotech::AnalysisLinkData.new.import
       Envirotech::ProviderData.new.import
-      Envirotech::RegulationData.new.import
-      Envirotech::SolutionData.new.import
+
+      scraper_data = Envirotech::ToolkitScraper.new.all_issue_info
+
+      Envirotech::RegulationData.new(scraper_data).import
+      Envirotech::SolutionData.new(scraper_data).import
       Envirotech::ProviderSolutionData.new.import
     end
   end
