a622cc889a7f22f727a9d9fef459abe4f14f90ed
baraabourghli <baraa.bourghli@gmail.com>
2015-08-16 22:59:50 +0800

    Specs for relation_data importing

--- spec/importers/envirotech/regulation_data_spec.rb
+++ spec/importers/envirotech/regulation_data_spec.rb
@@ -3,8 +3,12 @@ require 'spec_helper'
 describe Envirotech::RegulationData do
   include_context 'empty Envirotech indices'
   let(:fixtures_file) { "#{Rails.root}/spec/fixtures/envirotech/regulation_articles/regulation_articles.json" }
-  let(:importer) { described_class.new(fixtures_file) }
   let(:articles_hash) { YAML.load_file("#{File.dirname(__FILE__)}/regulation/regulation_articles.yaml") }
+
+  let(:relational_fixtures_file) { "#{Rails.root}/spec/fixtures/envirotech/relations_data/issue_solution_regulation.json" }
+  let(:relational_data) { JSON.parse(open(relational_fixtures_file).read) }
+
+  let(:importer) { described_class.new(fixtures_file, relation_data: relational_data) }
 
   it_behaves_like 'an importer which can purge old documents'
 
--- spec/importers/envirotech/solution_data_spec.rb
+++ spec/importers/envirotech/solution_data_spec.rb
@@ -3,8 +3,12 @@ require 'spec_helper'
 describe Envirotech::SolutionData do
   include_context 'empty Envirotech indices'
   let(:fixtures_file) { "#{Rails.root}/spec/fixtures/envirotech/solution_articles/solution_articles.json" }
-  let(:importer) { described_class.new(fixtures_file) }
   let(:articles_hash) { YAML.load_file("#{File.dirname(__FILE__)}/solution/solution_articles.yaml") }
+
+  let(:relational_fixtures_file) { "#{Rails.root}/spec/fixtures/envirotech/relations_data/issue_solution_regulation.json" }
+  let(:relational_data) { JSON.parse(open(relational_fixtures_file).read) }
+ 
+  let(:importer) { described_class.new('dummy_resource', relation_data: relational_data) }
 
   it_behaves_like 'an importer which can purge old documents'
 
