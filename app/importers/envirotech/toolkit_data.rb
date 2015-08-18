b7723eae196a9a70ce9039fb53372b10ae4af0a6
Calum Halcrow <calum@rrsoft.co>
2015-08-18 16:30:04 +0800

    fetch_relational_data method.

--- app/importers/envirotech/toolkit_data.rb
+++ app/importers/envirotech/toolkit_data.rb
@@ -3,27 +3,29 @@ module Envirotech
     RELATION_DATA = "#{Rails.root}/data/envirotech/issue_solution_regulation.json"
 
     def import
+      relational_data = fetch_relational_data
+
       Envirotech::IssueData.new.import
       Envirotech::BackgroundLinkData.new.import
       Envirotech::AnalysisLinkData.new.import
       Envirotech::ProviderData.new.import
+      Envirotech::RegulationData.new(relation_data: relational_data).import
+      Envirotech::SolutionData.new(relation_data: relational_data).import
+      Envirotech::ProviderSolutionData.new.import
+    end
 
-      # FIXME: this solution is valid until we have full control over the toolkit
-      local_data = JSON.parse(open(RELATION_DATA).read)
-      scraper_data = Envirotech::ToolkitScraper.new.all_issue_info
+    # This solution is valid until we have full control over the toolkit.
+    def fetch_relational_data
+      local = JSON.parse(open(RELATION_DATA).read).deep_symbolize_keys
+      scraped = Envirotech::ToolkitScraper.new.all_issue_info
 
-      if scraper_data.present? && scraper_data.deep_stringify_keys != local_data
+      if scraped.blank?
+        Airbrake.notify(Exceptions::EnvirotechToolkitNotFound.new)
+      elsif scraped != local
         Airbrake.notify(Exceptions::EnvirotechToolkitDataMismatch.new)
       end
 
-      if scraper_data.blank?
-        scraper_data = local_data
-        Airbrake.notify(Exceptions::EnvirotechToolkitNotFound.new)
-      end
-
-      Envirotech::RegulationData.new(relation_data: scraper_data).import
-      Envirotech::SolutionData.new(relation_data: scraper_data).import
-      Envirotech::ProviderSolutionData.new.import
+      scraped || local
     end
   end
 end
