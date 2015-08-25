27f3bb9dec48f660b5218fa2ab7fa63fed50b7b4
baraabourghli <baraa.bourghli@gmail.com>
2015-08-26 02:03:37 +0800

    import envirotech sources sequentially

--- app/importers/envirotech.rb
+++ app/importers/envirotech.rb
@@ -1,3 +1,17 @@
 module Envirotech
   include CanImportAllSources
+
+  def self.import_all_sources
+    Envirotech::IssueData.new.import
+    Envirotech::BackgroundLinkData.new.import
+    Envirotech::AnalysisLinkData.new.import
+    Envirotech::ProviderData.new.import
+
+    relational_data = Envirotech::ToolkitData.fetch_relational_data
+
+    Envirotech::RegulationData.new(relation_data: relational_data).import
+    Envirotech::SolutionData.new(relation_data: relational_data).import
+    Envirotech::ProviderSolutionData.new.import
+    true
+  end
 end
