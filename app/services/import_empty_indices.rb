a5e4c5131a7e9902a0c73344647b131d1507b5df
Calum Halcrow <calum@rrsoft.co>
2015-08-30 14:36:22 +0800

    Make dry_run a named argument to ImportEmptyIndices#call

--- app/services/import_empty_indices.rb
+++ app/services/import_empty_indices.rb
@@ -2,9 +2,7 @@ class ImportEmptyIndices
   class << self
     # Imports all model classes which have empty indices.
     # Returns lists of model classes it imported.
-    def call(options = {})
-      dry_run = options.deep_symbolize_keys.fetch(:dry_run, false)
-
+    def call(dry_run: false)
       Webservices::Application.model_classes.select do |model_class|
         search = ES.client.search(index: model_class.index_name, type: model_class.index_type)
         importer_class = model_class.importer_class
