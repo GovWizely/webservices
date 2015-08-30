e3d0dcef3cc08086ff332c7df57d0fc2155b9f39
Calum Halcrow <calum@rrsoft.co>
2015-08-30 14:25:03 +0800

    Skip Envirotech in ImportEmptyIndices

--- config/initializers/envirotech.rb
+++ config/initializers/envirotech.rb
@@ -1,3 +1,3 @@
-Envirotech::Login.username = '-'
-Envirotech::Login.password = '-'
-Envirotech::Login.web_auth = '-'
+Envirotech::Login.username = ''
+Envirotech::Login.password = ''
+Envirotech::Login.web_auth = ''
