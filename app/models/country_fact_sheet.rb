e23c4478388a4b7a19eb3443ec69f77a29ebbdae
Victor Lim <victor@rrsoft.co>
2015-10-07 18:35:37 +0800

    add es mapping to country fact sheet

--- app/models/country_fact_sheet.rb
+++ app/models/country_fact_sheet.rb
@@ -1,8 +1,41 @@
 class CountryFactSheet
   include Indexable
 
-  self.settings = {}
-  self.mappings = {}
+  self.settings = {
+    index: {
+      analysis: {
+        analyzer:
+                  { custom_analyzer:        {
+                    tokenizer: 'standard',
+                    filter:    %w(standard asciifolding lowercase snowball) },
+                    title_keyword_analyzer: {
+                      tokenizer: 'keyword',
+                      filter:    %w(asciifolding lowercase) },
+            },
+      },
+    },
+  }.freeze
+
+  self.mappings = {
+    name.typeize => {
+      _timestamp: {
+        enabled: true,
+        store:   true,
+      },
+      dynamic:    'false',
+      properties: {
+        title:         { type: 'string', analyzer: 'title_keyword_analyzer' },
+        site_url:      { type: 'string', analyzer: 'not_analyzed' },
+        content_url:   { type: 'string', analyzer: 'not_analyzed' },
+        full_url:      { type: 'string', analyzer: 'not_analyzed' },
+        mobile_url:    { type: 'string', analyzer: 'not_analyzed' },
+        bureau:        { type: 'string', analyzer: 'title_keyword_analyzer' },
+        official_name: { type: 'string', analyzer: 'title_keyword_analyzer' },
+        content_html:  { type: 'string', analyzer: 'standard' },
+        full_html:     { type: 'string', analyzer: 'standard' },
+      },
+    },
+  }.freeze
 
   self.source = {
     full_name: 'CountryFactSheet',
