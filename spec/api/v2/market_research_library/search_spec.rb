4eaa8d7e3f81f3518f59397d3f5de2af60459303
Victor Lim <victor@rrsoft.co>
2015-10-09 19:05:03 +0800

    add spec to reproduce market research library industry filter bug

--- spec/api/v2/market_research_library/search_spec.rb
+++ spec/api/v2/market_research_library/search_spec.rb
@@ -149,4 +149,18 @@ describe 'Market Researches API V2', type: :request do
       expect(json_response['total']).to eq(6)
     end
   end
+
+  describe 'GET /market_resarch_library/search.json' do
+    let(:params) { { industries: 'Construction, Building &Heavy Equipment' } }
+
+    before { get search_path, params, @v2_headers }
+    subject { response }
+
+    it_behaves_like 'a successful search request'
+
+    it 'returns all market researches and ignore query params' do
+      json_response = JSON.parse(response.body)
+      expect(json_response['total']).to eq(1)
+    end
+  end
 end
