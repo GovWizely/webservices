f42a095f23d406c07b8a773ae747a7ec0ad94598
Victor Lim <victor@rrsoft.co>
2015-10-12 18:19:55 +0800

    update trade lead analyzers naming and mappings

--- app/models/trade_lead/mappable.rb
+++ app/models/trade_lead/mappable.rb
@@ -1,22 +1,8 @@
 module TradeLead
   module Mappable
     def self.included(klass)
-      klass.settings = {
-        index: {
-          analysis: {
-            analyzer: {
-              custom_analyzer:       {
-                tokenizer: 'standard',
-                filter:    %w(standard asciifolding lowercase snowball),
-              },
-              lead_keyword_analyzer: {
-                tokenizer: 'keyword',
-                filter:    %w(asciifolding lowercase),
-              },
-            },
-          },
-        },
-      }.freeze
+      klass.analyze_by :snowball_asciifolding_nostop, :keyword_asciifolding_lowercase
+      klass.settings.freeze
 
       klass.mappings = {
         klass.name.typeize => {
@@ -26,19 +12,19 @@ module TradeLead
           },
           properties: {
             country:                  { type: 'string', analyzer: 'keyword' },
-            description:              { type: 'string', analyzer: 'custom_analyzer' },
+            description:              { type: 'string', analyzer: 'snowball_asciifolding_nostop' },
             contract_number:          { type: 'string', index: 'not_analyzed' },
             reference_number:         { type: 'string', index: 'not_analyzed' },
             industry:                 { type:   'string',
                                         fields: {
-                                          tokenized: { type: 'string', analyzer: 'custom_analyzer' },
+                                          tokenized: { type: 'string', analyzer: 'snowball_asciifolding_nostop' },
                                           keyword:   { type: 'string', analyzer: 'keyword' },
                                         },
                                       },
             title:                    {
-              type: 'string', analyzer: 'custom_analyzer',
+              type: 'string', analyzer: 'snowball_asciifolding_nostop',
               fields: {
-                keyword: { type: 'string', analyzer: 'lead_keyword_analyzer' },
+                keyword: { type: 'string', analyzer: 'keyword_asciifolding_lowercase' },
                 raw:     { type: 'string', index: 'not_analyzed' },
               }
             },
@@ -46,9 +32,9 @@ module TradeLead
             publish_date_amended:     { type: 'date', format: 'YYYY-MM-dd' },
             end_date:                 { type: 'date', format: 'YYYY-MM-dd' },
             specific_location:        { type: 'string', analyzer: 'standard' },
-            tags:                     { type: 'string', analyzer: 'custom_analyzer' },
-            implementing_entity:      { type: 'string', analyzer: 'custom_analyzer' },
-            procurement_organization: { type: 'string', analyzer: 'custom_analyzer' },
+            tags:                     { type: 'string', analyzer: 'snowball_asciifolding_nostop' },
+            implementing_entity:      { type: 'string', analyzer: 'snowball_asciifolding_nostop' },
+            procurement_organization: { type: 'string', analyzer: 'snowball_asciifolding_nostop' },
             source_lead:              { type: 'string', analyzer: 'keyword' },
             source:                   { type: 'string', analyzer: 'keyword' },
             categories:               { type: 'string', analyzer: 'keyword' },
