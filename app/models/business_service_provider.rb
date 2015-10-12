a7e86d9fae7f898ea649888cbd5ed281b3ca16fe
Victor Lim <victor@rrsoft.co>
2015-10-12 17:43:29 +0800

    update business service provider mappings analyzer

--- app/models/business_service_provider.rb
+++ app/models/business_service_provider.rb
@@ -1,6 +1,6 @@
 class BusinessServiceProvider
   include Indexable
-  analyze_by :snowball_asciifolding_nostop, :keyword_lowercase
+  analyze_by :snowball_asciifolding_nostop, :keyword_asciifolding_lowercase
 
   self.settings.freeze
 
@@ -19,10 +19,10 @@ class BusinessServiceProvider
         company_website:     { type: 'string', index: 'not_analyzed' },
         company_description: { type: 'string', analyzer: 'snowball_asciifolding_nostop' },
         company_email:       { type: 'string', index: 'not_analyzed' },
-        ita_office:          { type: 'string', analyzer: 'keyword_lowercase' },
+        ita_office:          { type: 'string', analyzer: 'keyword_asciifolding_lowercase' },
         contact_title:       { type: 'string', index: 'not_analyzed' },
         contact_name:        { type: 'string', analyzer: 'snowball_asciifolding_nostop' },
-        category:            { type: 'string', analyzer: 'keyword_lowercase' },
+        category:            { type: 'string', analyzer: 'keyword_asciifolding_lowercase' },
       },
     },
   }.freeze
