d2dcad9be0c13c3741e6bce06ea1702751dc31ab
Tim Hammer <timh@govwizely.com>
2015-10-16 11:48:39 -0400

    Usa.gov URLs for Market Research

--- spec/importers/market_research_data_spec.rb
+++ spec/importers/market_research_data_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe MarketResearchData, vcr: { cassette_name: 'industry_mapping_client/market_research.yml' } do
+describe MarketResearchData, vcr: { cassette_name: 'importers/market_research.yml', record: :once } do
   let(:resource) { "#{Rails.root}/spec/fixtures/market_research/source.txt" }
   let(:importer) { MarketResearchData.new(resource) }
   let(:expected) { YAML.load_file("#{File.dirname(__FILE__)}/market_research/expected_indexed_data.yaml") }
--- spec/support/common_endpoints_data_contexts.rb
+++ spec/support/common_endpoints_data_contexts.rb
@@ -1,7 +1,7 @@
 shared_context 'MarketResearch data' do
   before(:all) do
     MarketResearch.recreate_index
-    VCR.use_cassette('industry_mapping_client/market_research.yml', record: :once) do
+    VCR.use_cassette('importers/market_research.yml', record: :once) do
       MarketResearchData.new("#{Rails.root}/spec/fixtures/market_research/source.txt").import
     end
   end
