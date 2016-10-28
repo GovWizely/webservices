f5b53c60d87020ee1c58d10ff6975296775839d3
James Isaacs <james@govwizely.com>
2016-10-28 14:26:09 -0700

    Data source field name change

--- app/importers/screening_list/isa_data.rb
+++ app/importers/screening_list/isa_data.rb
@@ -4,6 +4,6 @@ module ScreeningList
       'https://www.treasury.gov/ofac/downloads/consolidated/consolidated.xml'
     self.source_information_url =
       'https://www.treasury.gov/resource-center/sanctions/programs/pages/iran.aspx#isa'
-    self.program_id = 'NS-ISA'
+    self.program_id = 'IRAN'
   end
 end
