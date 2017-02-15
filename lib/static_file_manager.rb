aa3c133734cf979f2c795aad05c802ec7bdf83ed
Loren Siebert <loren@siebert.org>
2017-02-15 09:30:41 -0800

    Simplified StaticFileManager tests and class methods

--- lib/static_file_manager.rb
+++ lib/static_file_manager.rb
@@ -3,8 +3,8 @@ require 'aws-sdk-core'
 class StaticFileManager
   CREDENTIALS = Aws::Credentials.new(Rails.configuration.aws_credentials[:access_key_id], Rails.configuration.aws_credentials[:secret_access_key])
 
-  def self.upload_all_files(search_class, s3 = nil)
-    s3_client = get_s3_client(s3)
+  def self.upload_all_files(search_class)
+    s3_client = get_s3_client
 
     search_data = search_class.fetch_all[:hits]
     %w(csv tsv).each do |format|
@@ -13,14 +13,13 @@ class StaticFileManager
     end
   end
 
-  def self.download_file(file_name, s3 = nil)
-    s3_client = get_s3_client(s3)
-    s3_client.get_object(bucket: 'search-api-static-files', key: file_name)
+  def self.download_file(file_name)
+    get_s3_client.get_object(bucket: 'search-api-static-files', key: file_name)
   end
 
   private
 
-  def self.get_s3_client(s3)
-    s3 || Aws::S3::Client.new(region: 'us-east-1', credentials: CREDENTIALS)
+  def self.get_s3_client()
+    Aws::S3::Client.new(region: 'us-east-1', credentials: CREDENTIALS)
   end
 end
