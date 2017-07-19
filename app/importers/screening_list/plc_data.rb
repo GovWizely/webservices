f1b24b01892b5d4f4b391ef675a71a4acbcb602a
James Isaacs <james@govwizely.com>
2017-07-19 15:48:21 -0600

    Update PLC source information URL

--- app/importers/screening_list/plc_data.rb
+++ app/importers/screening_list/plc_data.rb
@@ -3,7 +3,7 @@ module ScreeningList
     self.default_endpoint =
       'https://www.treasury.gov/ofac/downloads/consolidated/consolidated.xml'
     self.source_information_url =
-      'https://www.treasury.gov/resource-center/sanctions/Terrorism-Proliferation-Narcotics/Pages/index.aspx'
+      'https://www.treasury.gov/resource-center/sanctions/Programs/Pages/pa.aspx'
     self.program_id = 'NS-PLC'
   end
 end
